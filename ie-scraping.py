1bd23cddcffd8ca15cae20910d22279caeaad0ec
mstavens <manonop2016@gmail.com>
2022-03-23 11:24:31 -0700

    setup proxy for lenouvelliste

--- ie-scraping.py
+++ ie-scraping.py
@@ -3,7 +3,16 @@ import random
 import pandas as pd
 from bs4 import BeautifulSoup as bs
 
-free_proxy_url = '<http://proxy_list>' # TODO
+
+
+scheme = 'https'
+target_host= 'lenouvelliste.com'
+target_url= f'{scheme}://www.{target_host}'
+print (target_url)
+
+free_proxy_url='https://github.com/clarketm/proxy-list/blob/master/proxy-list-raw.txt'  # TODO
+
+
 
 # get the proxy as http response
 # TODO
@@ -11,46 +20,61 @@ free_proxy_url = '<http://proxy_list>' # TODO
 # Convert the http response to list of proxy
 # TODO
 
-def get_random_proxy(array):
-	''' get a random proxy '''
-	proxy = random.choice(array)
+#def get_random_proxy(array):
+#	''' get a random proxy '''
+#	proxy = random.choice(array)
 
 	# to get more info on the proxy format
 	# More info, visit: https://docs.python-requests.org/en/latest/api/#module-requests
-	return # {'protocol': 'ip:port'} # TODO
+#	return # {'protocol': 'ip:port'} # TODO
 
-def get_working_proxy():
-	''' test random proxies, to get one that works'''
-	TOTAL_TESTS = 20
-	for _ in range(TOTAL_TESTS):
-		proxy = get_random_proxy(<proxy_list>) # TODO
-		try:
-			requests.get('<test_url>', proxies=proxy) # TODO
-			# if it works, return it
-			return proxy
-		except Exception as error:
-			print(error)
+def get_random_proxy (proxy_list):
+    proxy = random.choice(proxy_list)
+    return{
+        'http':proxy,
+    }
+
+def get_working_proxy ():
+    res = requests.get(free_proxy_url)
+    proxy_list= res.text.strip().split('\n')
+    
+    total_tries =20
+    print("Getting working proxy...")
+    for _ in range (total_tries):
+        proxy= get_random_proxy(proxy_list)
+        print ('%s) Trying proxy %s' % (_ +1, proxy))
+        try:
+            res = requests.get('https://google.com', proxies=proxy, timeout=3)
+            if res.status_code==200:
+                print("One proxy found %s" % proxy)
+                return proxy
+        except:
+            print ("Not good\n")
+    print(f'we have tried {total_tries} times(s), but no working proxy found')
 
 
 # set a custom header
 headers = {
-	'User-Agent': '<browser>' # TODO
+	'Host': target_host,
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4501.0 Safari/537.36 Edg/91.0.866.0',
+    'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9, image/avif,image/webp,*/*;q=0.8',
+    'Accept-Language':'en-US,en;q=0.5',
+    'Accept-Encoding':'gzip, deflate, br', # TODO
 }
 
 # call and get the proxy
 proxy = get_working_proxy()
 if proxy:
-
-	# hit the target site
-	res = requests.get('<target_site>', headers=headers, proxies=proxy) # TODO
-
-	# init the beautiful instance as html parser
-	soup = bs(<target_content> , '<parser>') # TODO
-
-	# After collecting the data needed, convert it to pandas dataframe
-  # TODO
-
-	# then export it as csv
-	df.to_csv('<path/file_name>') # TODO
-else:
-	print('No working proxy found. Go buy some instead')
+ try:
+     print('Quering %s...'%target_url)
+     response = requests.get(target_url, headers=headers, proxies=proxy)
+     response.encoding=response.apparent_encoding
+     if response.status_code==200:
+         print ('Status code :200')
+         
+         soup = bs (response.text, 'html.parser')
+         print('Working')
+     else: 
+         print('Resquest failed with status code', response.status_code)
+ except Exception as err:
+     print('Request failed', err)
---
+++ web_scraping_test.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 22 08:41:47 2022
+
+@author: Mano
+"""
+
+import requests
+from bs4 import BeautifulSoup as bs
+import random
+
+scheme = 'https'
+target_host= 'kwenpam.com'
+target_url= f'{scheme}://www.{target_host}'
+print (target_url)
+proxy_url='https://github.com/clarketm/proxy-list/blob/master/proxy-list-raw.txt'
+
+
+headers ={
+    'Host': target_host,
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4501.0 Safari/537.36 Edg/91.0.866.0',
+    'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9, image/avif,image/webp,*/*;q=0.8',
+    'Accept-Language':'en-US,en;q=0.5',
+    'Accept-Encoding':'gzip, deflate, br',
+    }
+
+def get_random_proxy (proxy_list):
+    proxy = random.choice(proxy_list)
+    return{
+        'http':proxy,
+        }
+
+def get_working_proxy ():
+    res = requests.get(proxy_url)
+    proxy_list= res.text.strip().split('\n')
+    
+    total_tries =20
+    print("Getting working proxy...")
+    for _ in range (total_tries):
+        proxy= get_random_proxy(proxy_list)
+        print ('%s) Trying proxy %s' % (_ +1, proxy))
+        try:
+            res = requests.get('https://google.com', proxies=proxy, timeout=3)
+            if res.status_code==200:
+                print("One proxy found %s" % proxy)
+                return proxy
+        except:
+            print ("Not good\n")
+    print(f'we have tried {total_tries} times(s), but no working proxy found')
+
+proxy= get_working_proxy()
+
+if proxy:  
+    try:
+        print('Quering %s...'%target_url)
+        response = requests.get(target_url, headers=headers, proxies=proxy)
+        response.encoding=response.apparent_encoding
+        if response.status_code==200:
+            print ('Status code :200')
+            
+            soup = bs (response.text, 'html.parser')
+            print('Working')
+        else: 
+            print('Resquest failed with status code', response.status_code)
+    except Exception as err:
+        print('Request failed', err)
+        
+        
